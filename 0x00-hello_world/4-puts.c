bd9409b6d3f890f782b87b3f722f5ed4f87d6db0
mohamed <mohamed@gmail.com>
2023-08-10 16:25:28 -0700

    added some of changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * Description : will print the string 
+ * and return(0);
+ *
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
