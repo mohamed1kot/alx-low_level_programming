36902be0745a2b653d7e522ebfa38f2cda7c9de5
mohamed <mohamed@gmail.com>
2023-08-10 16:35:11 -0700

    added some of changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * Description : will print the string 
- * and return(0);
- *
- */
 
-int main()
+/**
+ * Description : will print the string
+ *
+ * and return(0);
+*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
