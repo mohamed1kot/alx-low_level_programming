93a0a4a43f540df0fc2531d9388b2bea9833e8fa
mohamed <mohamed@gmail.com>
2023-08-11 13:03:58 -0700

    add some of files

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * description: A C program Write a different combinations of two digits
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int set1 = 0;
+	int set2;
+
+	while (set1 <= 9)
+	{
+		set2 = 0;
+		while (set2 <= 9)
+		{
+			if (set1 != set2 && set1 < set2)
+				putchar(set1 + 48);
+				putchar(set2 + 48);
+				if (set1 != 8 || set2 != 9)
+					putchar(',');
+					putchar(' ');
+			set2++;
+		}
+		set1++;
+	}
+	putchar('\n');
+}
