a855fa0af5c2a9e6ea86c8128c333bf948cfe21e
mohamed <mohamed@gmail.com>
2023-08-10 19:00:58 -0700

    added files script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C Program print massege without printf and Puts
+ *
+ * Return: Always 1 (success)
+*/
+
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, s, sizeof(s) - 1);
+	return (1);
+}
