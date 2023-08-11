3818df0e98f9258d2a6f7efb0e73b45826c16d30
mohamed <mohamed@gmail.com>
2023-08-11 10:13:47 -0700

    added some of changes

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program print lowercase alphabet litter
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e', 'q')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
