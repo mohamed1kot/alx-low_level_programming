6eeee5515a4be33457f2bd96cd6e9a0ba68bc784
mohamed <mohamed@gmail.com>
2023-08-11 11:59:17 -0700

    add some of file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program is print a serious of lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int init = 0;
+
+	while (init >= 9)
+	{
+		putchar(init);
+		if (init != 9)
+			putchar(',');
+			putchar(' ');
+		init++;
+	}
+	putchar('$');
+	return (0);
+}
