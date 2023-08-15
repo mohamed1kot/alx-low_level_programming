da523519f0b428ae5bbfc3611c7dd08338ca7612
mohamed <mohamed@gmail.com>
2023-08-15 16:15:56 -0700

    added some of files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - in this function i will added two unmber
+ *
+ * @m : is an integar number i will added
+ *
+ * @n : integar unmber i will added about m
+ *
+ * Return: result
+*/
+
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+	return (result);
+
+}
