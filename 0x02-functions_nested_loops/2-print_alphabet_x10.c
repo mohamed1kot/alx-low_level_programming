0c2a1ab53612aa88a0e377ae956d415d28de1d9f
mohamed <mohamed@gmail.com>
2023-08-15 12:19:57 -0700

    added some of file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this function print alphapt from "a" to "z" 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
