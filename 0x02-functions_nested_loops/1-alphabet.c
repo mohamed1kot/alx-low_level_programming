d1563dbbbaa6d60572ef89b26f04fa695f43579b
mohamed <mohamed@gmail.com>
2023-08-15 12:00:15 -0700

    added some of files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is a function is print alphapt later from "a" to "z"
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
