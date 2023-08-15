475351617f27aaab4b99d25ef14f7d0c8a1469f8
mohamed <mohamed@gmail.com>
2023-08-15 13:31:08 -0700

    added s new file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - check if n>0 or n<0 or equal
+ *
+ * @n: intager input
+ *
+ * Return: (1)if n>0 (0)if n==0 (-1)if n<0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
