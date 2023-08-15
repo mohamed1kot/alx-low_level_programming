a0be5a5b5864bfc38b7258f28394cc891a9b3283
mohamed <mohamed@gmail.com>
2023-08-15 15:50:30 -0700

    added some of files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,8 +19,12 @@ void times_table(void)
 		_putchar(48);
 		for (num2 = 1; num2 <= 9; num2++)
 		{
+			if (num2 < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			multi = num1 * num2;
-
 			if (multi <= 9)
 			{
 				_putchar(' ');
