baeeae6dbb99fe5ba7fddc6d020e77abaac9b264
mohamed <mohamed@gmail.com>
2023-08-15 17:05:58 -0700

    added some of files

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - in this program print unmbers from n to 98
+ *
+ * @n: input number i started from it
+ *
+ * Return: no return
+*/
+
+void print_to_98(int n)
+{
+	int temp;
+
+	if (n > 98)
+	{
+		for (temp = n; temp >= 98; temp--)
+		{
+			printf("%d, ", temp);
+		}
+	}
+	else if (n < 98)
+	{
+		for (temp = n; temp <= 98; temp++)
+		{
+			printf("%d, ", temp);
+		}
+	}
+	else
+	{
+		printf("98\n");
+	}
+}
