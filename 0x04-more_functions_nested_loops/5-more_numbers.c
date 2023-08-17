194a04466886f0d7d3045b0d7c78f2e7662a5498
mohamed <mohamed@gmail.com>
2023-08-17 13:55:08 -0700

    added some of files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers -print numbers from 0 to 14 as 10 times
+ *
+ * Return: none
+*/
+
+void more_numbers(void)
+{
+	int times;
+	int num;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num <= 9)
+			{
+				_putchar(num + 48);
+			}
+			else
+			{
+				_putchar((num / 10) + 48);
+				_putchar((num % 10) + 48);
+			}
+		}
+	}
+}
