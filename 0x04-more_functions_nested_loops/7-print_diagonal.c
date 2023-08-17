a95668eb101226ff1d4311772003a3ae0c98342f
mohamed <mohamed@gmail.com>
2023-08-17 16:12:50 -0700

    added some of files

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print ' ' in times
+ *
+ * @n : input times of ' ' you want to print it
+ *
+ * Return : none
+*/
+
+void print_diagonal(int n)
+{
+	int count1;
+	int count2;
+
+	for (count1 = 1; count1 <= n; count1++)
+	{
+		for (count2 = 1; count2 < count1; count2++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
