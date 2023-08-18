412761d2b7ff8cdd2e775a3f9aa8259141d24d25
mohamed <mohamed@gmail.com>
2023-08-17 17:22:12 -0700

    added some of files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-/***/
+/**
+ * main - Entry point
+ *
+ * description : print from 1 to 100
+ *
+ * Return: Always 0 (success)
+*/
 
 int main(void)
 {
@@ -8,11 +14,11 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count % 3 == 0 && count % 5 ==0)
+		if (count % 3 == 0 && count % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (count % 3 == 0 )
+		else if (count % 3 == 0)
 		{
 			printf("Fizz");
 		}
