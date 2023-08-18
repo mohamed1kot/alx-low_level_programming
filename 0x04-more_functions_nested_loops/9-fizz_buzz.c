c31a7b315a10a5dbf151d53823fb48f99434c40d
mohamed <mohamed@gmail.com>
2023-08-17 17:10:17 -0700

    some of changes

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/***/
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 ==0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (count % 3 == 0 )
+		{
+			printf("Fizz");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", count);
+		}
+		if (count < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
