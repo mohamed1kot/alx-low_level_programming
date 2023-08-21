a6693b2b5e282a7dd1d4bd93d72b52bf0bac8304
mohamed <mohamed@gmail.com>
2023-08-21 16:28:55 -0700

    added some of files

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi- Write a function that convert a string to an integer.
+ *
+ * @s : input
+ *
+ * Return: always (num * c)
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int c = 1;
+
+	do {
+		if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (*s == '-')
+			c *= -1;
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * c);
+}
