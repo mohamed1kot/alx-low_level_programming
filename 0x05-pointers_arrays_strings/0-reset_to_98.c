e9b321f3a8aecac2a3c88b4e3c0272434ea5c96d
mohamed <mohamed@gmail.com>
2023-08-21 14:21:41 -0700

    Write a function that swaps the values of two integers.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Write a function that swaps the values of two integers
+ *
+ * @n:  input
+ *
+ * Return : not return any thing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
