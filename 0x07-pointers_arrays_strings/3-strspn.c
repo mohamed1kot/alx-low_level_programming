6a44142f916191d7c71ccfa5b6272de7dec89ce6
mohamed <mohamed@gmail.com>
2023-08-28 13:18:03 -0700

    addded some of files

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - function calculates the length (in bytes) of the initial segment of s
+ *
+ * @s :string input will search in it
+ *
+ * @ accept : string input i will search about it
+ *
+ * Return: sum
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int sum;
+
+	sum = 1;
+	for (i = 0; accept[i] > '\0'; i++)
+	{
+		for (j = 0; s[j] > '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				sum += 1;
+				break;
+			}
+		}
+	}
+	return (sum);
+}
