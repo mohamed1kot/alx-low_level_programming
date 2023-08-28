91f053b29492d6b1ce6fc173b6920e325de5f5da
mohamed <mohamed@gmail.com>
2023-08-28 13:35:48 -0700

    addded some of files

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,21 +12,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	int j;
-	int sum;
+	unsigned int i;
+	unsigned int j;
 
-	sum = 1;
-	for (i = 0; accept[i] > '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] > '\0'; j++)
+		for (j = 0; accept[j] != s[i]; j++)
 		{
-			if (accept[i] == s[j])
+			if (accept[j] == '\0')
 			{
-				sum += 1;
-				break;
+				return (i);
 			}
 		}
 	}
-	return (sum);
+	return (i);
 }
