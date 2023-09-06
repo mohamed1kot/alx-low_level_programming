41ff565a2ce86eb56da06ba7013da314a000031f
mohamed <mohamed@gmail.com>
2023-09-06 03:41:56 +0300

    add some of changes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,24 @@
 char *_strdup(char *str)
 {
 	char *str1;
-	unsigned int size;
-	unsigned int i;
+	unsigned int size = 0;
+	unsigned int i = 0;	
 
-	for (size = 0; str[size] != '\0'; size++)
+	for (; str[size] != '\0'; size++)
 		;
 	if (str == NULL)
 		return (NULL);
 	str1 = malloc(size * sizeof(*str) + 1);
-	for (i = 0; i < size; i++)
+	if (str1 == 0)
 	{
-		str1[i] = str[i];
+		return (NULL);
+	}
+	else
+	{
+		for (; i < size; i++)
+		{
+			str1[i] = str[i];
+		}
 	}
 	return (str1);
 }
