ef33944e060b83d9046c35f397157ce8ca3054bf
mohamed <mohamed@gmail.com>
2023-09-06 05:49:06 +0300

    added some change in file str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,9 @@ char *str_concat(char *s1, char *s2)
 	int size2;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
