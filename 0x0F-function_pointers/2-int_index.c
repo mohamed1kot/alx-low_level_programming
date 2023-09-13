fd0c518f9921cd293a6b7a0f4ac3f585754ad9ff
mohamed <mohamed@gmail.com>
2023-09-13 21:02:56 +0300

    add some of changes in file 1-array_iterator.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (index = 0; index < size; index++)
 	{
