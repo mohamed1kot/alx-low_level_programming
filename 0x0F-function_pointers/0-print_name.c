481384d54429c9864b7106236d4dbf05d930a766
mohamed <mohamed@gmail.com>
2023-09-13 20:12:06 +0300

    added initial files in this project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ *
+ * @name : the name you would to print it.
+ * @f : is a pointer points to function.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
