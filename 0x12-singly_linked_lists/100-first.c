2b2b3f24fa8e086ad0a18408d20070a2cfe533ec
mohamed <mohamed@gmail.com>
2023-09-22 14:05:14 +0300

    add another one file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) _print_str(void);
+
+/**
+ * _print_str - a function that prints
+ * "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+ * before the main function is executed.
+ *
+ * Return: don`t return anything.
+*/
+
+void _print_str(void)
+{
+	printf("You're beat! and yet, you must allow
+			,\nI bore my house upon my back!\n");
+}
