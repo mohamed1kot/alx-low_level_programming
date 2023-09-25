828a5351e6a957e1ccc2e9ccba12d71bafbf46f1
mohamed <mohamed@gmail.com>
2023-09-25 07:31:42 +0300

    add anoyher on task

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - a function is add a new node
+ * at the begining of a listint_t list.
+ *
+ * @head : input the head of linked list.
+ * @n : a new elements will added it.
+ *
+ * Return: the address of the new element, or NULL if it failed.
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
