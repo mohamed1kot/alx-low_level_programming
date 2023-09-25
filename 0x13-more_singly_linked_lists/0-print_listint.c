94ad97f11580b7650d19d3bc6d3e15250623b73f
mohamed <mohamed@gmail.com>
2023-09-25 21:36:59 +0300

    add another change in file 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t C_node = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		C_node++;
 		printf("%d\n", h->n);
 		h = h->next;
-		C_node++;
 	}
 
 	return (C_node);
