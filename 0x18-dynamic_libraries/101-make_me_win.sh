69bb43d50616d1175ad1c4ec002fdce94113e789
mohamed <mohamed@gmail.com>
2023-12-11 21:11:44 +0200

    add another new file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/mohamed1kot/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=tmp/libgiga.so"
+wget -P /tmp https://github.com/mohamed1kot/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so
+export  /tmp/libdynamic.so
