8cdad79fdf058124c103cd32a7f72b18590f933f
mohamed <mohamed@gmail.com>
2023-12-12 02:36:47 +0200

    added another changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/mohamed1kot/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so
-export  /tmp/libdynamic.so
+export  LD_PRELOAD="/home/mohamed/Desktop/ALX_prgram/alx-low_level_programming/0x18-dynamic_libraries/libdynamic.so"
